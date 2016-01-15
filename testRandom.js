9ba73465459d7986f5ae389356f16231c79d1ee0
Peter Sorowka <sorowka@skybus.io>
2016-01-15 12:33:05 +0100

    Improved output of random test
    When a random test package triggers a unhandled parser exception, the
    content of this packet as well as the stack trace are printed in order
    to ease debugging

--- testRandom.js
+++ testRandom.js
@@ -1,21 +1,32 @@
 
 var mqtt    = require('./')
-  , crypto  = require('crypto')
-  , max     = 100000
-  , i
-  , start   = Date.now() / 1000
-  , time
-  , errors  = 0
-
+, crypto  = require('crypto')
+, max     = 100000
+, i
+, start   = Date.now() / 1000
+, time
+, errors  = 0
+, randomPacket
 
 function doParse () {
   var parser  = mqtt.parser()
   parser.on('error', onError)
-  parser.parse(crypto.randomBytes(Math.floor(Math.random() * 10)))
+  randomPacket = crypto.randomBytes(Math.floor(Math.random() * 10))
+  parser.parse(randomPacket)
 }
 
-for (i = 0; i < max; i++) {
-  doParse()
+try {
+
+  for (i = 0; i < max; i++) {
+    doParse()
+  }
+}
+catch(e) {
+
+  console.log('Exception occured at packet ')
+  console.log(new Buffer(randomPacket))
+  console.log(e.message)
+  console.log(e.stack)
 }
 
 function onError () {
